4c269dcbca7bb1ba71e605906db967183bf255a5
DanielaRios <daniela_erre@hotmail.com>
2025-11-19 21:15:48 -0300

    agregamos Morgan

--- src/server.js
+++ src/server.js
@@ -1,12 +1,16 @@
 import express from "express";
+import morgan from "morgan";
 import EmpleadoRouter from "./router/empleado.router.js";
 import apiExternaRouter from "./router/api.externa.router.js";
 
 //inicializamos express
 const server = express();
+const morgarnModule = morgan(':method :url :status :res[content-length] - :response-time ms');
+
 
 //servidor permite recibir e interpretar express a cualquier peticion que se le haga por JSON
 server.use(express.json());
+server.use(morgarnModule); //logging de las peticiones al server
 
 // middleware para api externa
 server.use("/api/capacitaciones-externas",apiExternaRouter);
